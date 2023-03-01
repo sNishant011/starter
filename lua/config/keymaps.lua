28f9b53e737c67bbe15ab0017165721d9dbeec91
sNishant011 <shresthanishant011@gmail.com>
2023-03-01 08:29:13 +0545

    feat: copilot, do not yank on replace

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.api.nvim_set_keymap
+keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+
+-- disable replace paste
+keymap("n", "x", '"_x', { silent = true })
+keymap("n", "X", '"_X', { silent = true })
+keymap("v", "x", '"_x', { silent = true })
+keymap("v", "X", '"_X', { silent = true })
+keymap("v", "p", '"_dP', { silent = true })
