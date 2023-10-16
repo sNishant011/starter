285fc2e7ab7a582e1e0d1f00b7259c0b2776b88e
sNishant011 <shresthanishant011@gmail.com>
2023-10-16 21:52:36 +0545

    feat: deno lsp setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,6 +6,22 @@ return {
         tsserver = function(_, opts)
           require("lazyvim.util").on_attach(function(client, buffer)
             if client.name == "tsserver" then
+              local active_clients = vim.lsp.get_active_clients()
+              if client.name == "denols" then
+                for _, client_ in pairs(active_clients) do
+                  -- stop tsserver if denols is already active
+                  if client_.name == "tsserver" then
+                    client_.stop()
+                  end
+                end
+              elseif client.name == "tsserver" then
+                for _, client_ in pairs(active_clients) do
+                  -- prevent tsserver from starting if denols is already active
+                  if client_.name == "denols" then
+                    client.stop()
+                  end
+                end
+              end
               client.server_capabilities.documentFormattingProvider = false
             end
             if client.name == "eslint" then
@@ -13,6 +29,21 @@ return {
             end
           end)
           require("typescript").setup({ server = opts })
+          return true
+        end,
+        denols = function(_, opts)
+          local nvim_lsp = require("lspconfig")
+
+          local on_attach = function(client, _)
+            if client.name == "denols" then
+              client.server_capabilities.documentFormattingProvider = true
+            end
+          end
+
+          nvim_lsp.denols.setup({
+            on_attach = on_attach,
+            root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+          })
           return true
         end,
       },
